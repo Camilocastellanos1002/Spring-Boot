1e3c50b2c99560e531476225b78025cbcde776f6
Juan Camilo <jccaste1002@gmail.com>
2024-04-16 09:36:00 -0500

    primera web

---
+++ _02_primeraPrueba/primerWeb/src/main/java/com/riwi/primerWeb/PrimerWebApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.primerWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerWebApplication.class, args);
+	}
+
+}
---
+++ _02_primeraPrueba/primerWeb/src/test/java/com/riwi/primerWeb/PrimerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.primerWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
