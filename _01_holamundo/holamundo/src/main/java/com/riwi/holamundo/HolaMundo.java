d61dc65e16c91fa13e2f95b1a036616c5fce2cc3
Juan Camilo <jccaste1002@gmail.com>
2024-04-16 07:54:30 -0500

    primeras anotaciones

--- _01_holamundo/holamundo/src/main/java/com/riwi/holamundo/HolaMundo.java
+++ _01_holamundo/holamundo/src/main/java/com/riwi/holamundo/HolaMundo.java
@@ -1,11 +1,24 @@
 package com.riwi.holamundo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-//crear anotacion y/o frijol
+
+
+//crear anotacion y/o frijol que indica que esta clase sera un controlador
 @Controller
+//RequestMapping crea la ruta base del controlador
+@RequestMapping("/controller")
 public class HolaMundo {
 
+    //GetMapping crea una ruta especifica para el metodo
+    @GetMapping("/holamundo")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
     public String mostrarMensaje(){
         return "Hola mundo";
     }
